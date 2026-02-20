d269e60aa260ae3d8147a39050c30b8c9459e57b
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2026-02-20 15:24:39 +0530

    Update principal.js

--- models/principal.js
+++ models/principal.js
@@ -1,40 +1,81 @@
 var mongoose = require("mongoose");
-// bcrypt ki ab zaroorat nahi hai
+
+var bcrypt = require("bcryptjs");
+
 var passportLocalMongoose = require("passport-local-mongoose");
 
+
+
 var principalSchema = new mongoose.Schema({
+
   name: String,
+
   type: String,
+
   username: String,
-  password: String, // Ye ab plain text store karega
+
+  password: String,
+
   hostel: String,
+
   image: String
+
 });
 
+
+
 principalSchema.plugin(passportLocalMongoose);
+
 var Principal = (module.exports = mongoose.model("Principal", principalSchema));
 
-// Naya principal create karne ke liye (Bina hashing ke)
+
+
 module.exports.createPrincipal = function(newPrincipal, callback) {
-    // Hashing logic hata diya gaya hai, direct save karein
-    newPrincipal.save(callback);
+
+  bcrypt.genSalt(10, function(err, salt) {
+
+    bcrypt.hash(newPrincipal.password, salt, function(err, hash) {
+
+        newPrincipal.password = hash;
+
+        newPrincipal.save(callback);
+
+    });
+
+  });
+
 };
+
+
 
 module.exports.getUserByUsername = function(username, callback) {
+
   var query = { username: username };
+
   Principal.findOne(query, callback);
+
 };
+
+
 
 module.exports.getUserById = function(id, callback) {
+
   Principal.findById(id, callback);
+
 };
 
-// Password compare karne ke liye (Direct match)
-module.exports.comparePassword = function(candidatePassword, storedPassword, callback) {
-  // Hash comparison ki jagah direct string match
-  if (candidatePassword === storedPassword) {
-      callback(null, true);
-  } else {
-      callback(null, false);
-  }
+
+
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+
+  bcrypt.compare(candidatePassword, hash, function(err, passwordFound) {
+
+    if (err) throw err;
+
+    callback(null, passwordFound);
+
+  });
+
 };
+
+esme kya chang kare
